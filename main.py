40fc8dbd1d54d715058fd7244dda2e4e2d02340f
anaysoul <soulyana_lakew@outlook.com>
2021-07-12 14:53:40 -0400

    print statement

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("Hello world!");
 print('Day 1 - Python Print Functions');
 print("The function is declared like this:");
 print("print('what to print')");
+
+print("Hello " + "Angela");
+
+# input function
