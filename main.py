85f964b7fd1ba0205dc6ec79b26736d49354d199
anaysoul <soulyana_lakew@outlook.com>
2021-07-13 10:47:57 -0400

    project 2 tip calculator

--- main.py
+++ main.py
@@ -49,22 +49,42 @@
 # print("Your rockstar band name is " + city + " " + pet + "!");
 
 # Project 2: Tip Calculator
-# print("Welcome to the tip calculator bot!");
-# total = input("What was the total bill?\n");
-# split = input("How many people to split the bill?\n");
-# tip = input("What percentage tip would you like to give? 10, 12, or 15?\n");
-# total_number = int(total);
-# split_number = int(split);
-# tip_number = int(tip);
-# # print(type(split));
-# # print(type(tip));
-# # print(type(total));
-# tip_per_person = (total_number / split_number) * (tip_number/100);
-# print("Each person should pay: $" + str(tip_per_person));
+print("Welcome to the tip calculator bot!");
+total = float(input("What was the total bill?\n"));
+split = int(input("How many people to split the bill?\n"));
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15?\n"));
+# print(type(split));
+# print(type(tip));
+# print(type(total));
+tip_amount = total* (tip/100);
+tip_per_person = (total + tip_amount) / split;
+tip_per_person = "{:.2f}".format(tip_per_person);
+print(f"Each person should pay: ${tip_per_person}");
 
 # Data Types Exercise
-two_digit_number = input("Type a two digit number: \n");
-a = int(two_digit_number[0]);
-b = int(two_digit_number[1]);
-ab = str(a + b);
-print(str(a) + " + " + str(b) + " = " + ab );
+# two_digit_number = input("Type a two digit number: \n");
+# a = int(two_digit_number[0]);
+# b = int(two_digit_number[1]);
+# ab = str(a + b);
+# print(str(a) + " + " + str(b) + " = " + ab );
+
+# BMI Calculator Exercise
+# height = float(input("enter your height in m: "));
+# weight = float(input("enter your weight in kg: "));
+
+# bmi = int(weight / (height ** 2));
+# print("Your BMI is " + str(bmi));
+
+# # f-string
+# score = 0;
+# height = 1.8
+# isWinning = True
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}");
+
+# Life in Weeks
+# age = int(input("What is your current age? \n"));
+# remaining_years = 90 - age;
+# days = (remaining_years * 365);
+# weeks = (remaining_years * 52);
+# months = (remaining_years * 12);
+# print(f"You have {days} days, {weeks} weeks and {months} months left!");
