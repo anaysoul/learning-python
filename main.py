18c5afd18bdc67d0f5c1939b6063c914d6ad9953
anaysoul <soulyana_lakew@outlook.com>
2021-07-16 09:42:06 -0400

    completed hangman project

--- main.py
+++ main.py
@@ -585,70 +585,17 @@
 
 
 import random
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_words import word_list
+from hangman_art import stages, logo 
 
 # my_function()
-word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 end_of_game = False
 word_length = len(chosen_word)
 lives = 6
+
+# print hangman logo
+print(logo)
 
 #Testing code
 print(f"Psst, the solution is {chosen_word}") 
@@ -663,6 +610,10 @@ print(display)
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    # notify user of repeating guesses
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -670,7 +621,7 @@ while not end_of_game:
             display[position] = guess
     if guess not in chosen_word:
         lives -= 1
-        print(f"Lives: {lives}")
+        print(f"Wrong guess: {guess}! Lives: {lives}")
         if lives == 0:
             end_of_game = True
             print("You lose!")
