e38943cfe9cf844c6f32dde1f4d14df3be9ee37a
anaysoul <soulyana_lakew@outlook.com>
2021-07-16 16:54:29 -0400

    functions with parameters

--- main.py
+++ main.py
@@ -584,51 +584,56 @@
 #     print("Bye")
 
 
-import random
-from hangman_words import word_list
-from hangman_art import stages, logo 
+# import random
+# from hangman_words import word_list
+# from hangman_art import stages, logo 
 
-# my_function()
-chosen_word = random.choice(word_list)
-end_of_game = False
-word_length = len(chosen_word)
-lives = 6
+# # my_function()
+# chosen_word = random.choice(word_list)
+# end_of_game = False
+# word_length = len(chosen_word)
+# lives = 6
 
-# print hangman logo
-print(logo)
+# # print hangman logo
+# print(logo)
 
-#Testing code
-print(f"Psst, the solution is {chosen_word}") 
+# #Testing code
+# print(f"Psst, the solution is {chosen_word}") 
 
-# Create blanks
-display = []
-for char in range(word_length):
-    display.append("_")
-print(display)
+# # Create blanks
+# display = []
+# for char in range(word_length):
+#     display.append("_")
+# print(display)
 
 
-while not end_of_game:
-    guess = input("Guess a letter: ").lower()
+# while not end_of_game:
+#     guess = input("Guess a letter: ").lower()
 
-    # notify user of repeating guesses
-    if guess in display:
-        print(f"You've already guessed {guess}")
+#     # notify user of repeating guesses
+#     if guess in display:
+#         print(f"You've already guessed {guess}")
 
-    #Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = guess
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"Wrong guess: {guess}! Lives: {lives}")
-        if lives == 0:
-            end_of_game = True
-            print("You lose!")
-    print(display)
-    if "_" not in display:
-        end_of_game = True
-        print("You win!")
-    # Display hangman
-    print(stages[lives])
+#     #Check guessed letter
+#     for position in range(word_length):
+#         letter = chosen_word[position]
+#         if letter == guess:
+#             display[position] = guess
+#     if guess not in chosen_word:
+#         lives -= 1
+#         print(f"Wrong guess: {guess}! Lives: {lives}")
+#         if lives == 0:
+#             end_of_game = True
+#             print("You lose!")
+#     print(display)
+#     if "_" not in display:
+#         end_of_game = True
+#         print("You win!")
+#     # Display hangman
+#     print(stages[lives])
 
+def greeting(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greeting(name="Jack Bauer", location="nowhere")
